fc554bfef38216142fa19afd7d3a81270a501f57
HyoEun Lee <hyoeun93@gmail.com>
2020-01-06 12:24:04 -0500

    addBinary solution1

--- addBinary.js
+++ addBinary.js
@@ -1,1 +1,10 @@
 //Given two binary strings, return their sum (also a binary string).
+/*
+Ex1:
+Input: a = "11", b = "1"
+Output: "100" 
+*/
+
+let addBinary = function(a, b) {
+    return (parseInt(a, 2) + parseInt(b, 2)).toString(2);
+}
