50681922bddfd6740555136c05a9727f3d1a2d2e
HyoEun Lee <hyoeun93@gmail.com>
2020-01-12 18:32:41 -0500

    strStr solution

--- strStr().js
+++ strStr().js
@@ -1,5 +1,16 @@
 //return the index of the first occurrence of needle in haystack, or -1 if needle is not part of haystack.
 
 var strStr = function(haystack, needle) {
+    let index = -1;
 
+    if(haystack.length && needle) {
+        for(let i = 0; i < haystack.length; i++) {
+            if(haystack[i] == needle[0]) {
+                if(haystack.substring(i, i + needle.length) == needle) {
+                    return i;
+                }
+            }
+        }
+    } else if (!needle) return 0;
+    return index;
 };
