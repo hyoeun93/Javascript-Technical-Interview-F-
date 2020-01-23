5bc9733a2b594e85a89a741fe67a346baaa097ab
HyoEun Lee <hyoeun93@gmail.com>
2020-01-22 22:27:02 -0500

    subarraySum

---
+++ subarraySum.js
@@ -0,0 +1,9 @@
+//Given a list of non-negative numbers and a target integer k, 
+//write a function to check if the array has a continuous subarray of size at least 2 
+//that sums up to a multiple of k, that is, sums up to n*k where n is also an integer.
+
+var checkSubarraySum = function(nums, k) {
+    let sum = 0;
+
+    if(sum % k === 0) return true;
+}
